820cbd07c09fc71f384b13001020c71352ce07a1
Lauren Baca <laurenbaca1895@gmail.com>
2022-11-02 18:08:42 -0400

    Randomly autoplays a sean mp3 when extension is clicked-- ver. 1.0.4

---
+++ main.js
@@ -0,0 +1,17 @@
+const seanUrl1 = '/public/debugging-woes.mp3';
+const seanUrl2 = '/public/promise.mp3';
+const seanUrl3 = '/public/touch-some-grass.mp3';
+const seanUrl4 = '/public/two-days-ago.mp3';
+
+const urlArray = [seanUrl1, seanUrl2, seanUrl3, seanUrl4];
+
+function getASean() {
+  const randomSean = urlArray[Math.floor(Math.random() * 4)];
+  console.log('randomSean: ', randomSean);
+
+  document.getElementById('sean-audio-source').setAttribute('src', randomSean);
+}
+
+console.log('this file is run!!');
+
+getASean();
